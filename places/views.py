8ca0bebd691de84193718097f0841efc9f4a5846
KirillYabl <kirill-yablunovskii@mail.ru>
2022-11-22 18:07:02 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
+import os
+from typing import Dict, Any
 
-# Create your views here.
+from django.shortcuts import render
+from django.conf import settings
+
+from .models import Place
+
+
+def write_json_by_place(place: Place) -> str:
+    json_file_data = {
+        "title": place.title,
+        "imgs": [image.place_image.url for image in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lon,
+            "lat": place.lat
+        }
+    }
+    filename = f'{place.pk}.json'
+    with open(os.path.join(settings.MEDIA_ROOT, filename), 'w', encoding='UTF-8') as f:
+        json.dump(json_file_data, f, ensure_ascii=False)
+
+    return f"./media/{filename}"
+
+
+def write_feature_by_place(place: Place) -> Dict[str, Any]:
+    feature = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [place.lon, place.lat]
+        },
+        "properties": {
+            "title": place.title,
+            "placeId": place.pk,
+            "detailsUrl": write_json_by_place(place)
+        }
+    }
+    return feature
+
+
+def index(request):
+    places_geojson = {
+        'type': 'FeatureCollection',
+        'features': [write_feature_by_place(place) for place in Place.objects.all()]
+    }
+    context = {'places_geojson': places_geojson}
+    return render(request, 'index.html', context)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', views.index, name='index'),
+                  path('', include('places.urls')),
               ] + \
               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
