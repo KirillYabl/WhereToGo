5cc9f958c2bc24d95a4aca37dd132d4628482448
KirillYabl <kirill-yablunovskii@mail.ru>
2022-11-23 16:44:11 +0300

    Add tinymce to admin in Place.description_long field

--- places/models.py
+++ places/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 # Create your models here.
 class Place(models.Model):
     title = models.CharField(verbose_name='название', max_length=500)
     description_short = models.TextField(verbose_name='краткое описание')
-    description_long = models.TextField(verbose_name='полное описание', blank=True, null=True)
+    description_long = HTMLField(verbose_name='полное описание', blank=True, null=True)
     lat = models.FloatField(verbose_name='широта')
     lon = models.FloatField(verbose_name='долгота')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('tinymce/', include('tinymce.urls')),
                   path('', include('places.urls')),
               ] + \
               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
